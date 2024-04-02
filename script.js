97ed88dace7543e68211ebdd2210bf16cc7a4cc4
Lisa Donohoo <lisa.donohoo@gmail.com>
2024-04-01 20:12:04 -0500

    updateGameData working

--- script.js
+++ script.js
@@ -1,9 +1,53 @@
+/**
+ * Lisa Donohoo
+ * Prime Digital Academy
+ * Tier 2: Fungus-Fighter
+ * 
+ * 
+ * when attack button:
+ *  - update hp 
+ *  - update ap
+ * 
+ *  - get values of hp/ap for attack
+ *  - reset negative values to 0
+ * 
+ * render state changes to DOM
+ *  - update hp/ap text above attack buttons
+ *  - if fungus = 0 hp:
+ *      walk ---> dead class on freaky-fungus element
+ *  - if your ap = 0: 
+ *      -walk ---> jump class on freaky-fungus element
+ *      - all attack buttons get disabled attribute
+ * 
+ *  - update progress element value for input fields
+ * 
+ *  - if freaky-fungus hp < 50, regen 1 hp every second
+ *          with setInterval()
+ */
+
+
+
 // State Variables can be declared outside of the onReady
 // Feel free to make this to what you want!
 // Example:
 // let fungusHP = 100;
-let fungusHP = 100;
-let attackPoints = 100;
+let fungusGameData = {
+    fungusHP : 100,
+    attackPoints : 100,
+    updateGameData: function(cost, damage) {
+        // update HP and AP
+        this.fungusHP -= damage;
+        this.attackPoints -= cost;
+        // minimum HP & AP values should be 0
+        if (this.fungusHP < 0) {
+            this.fungusHP = 0;
+        }
+        if (this.attackPoints < 0) {
+            this.attackPoints = 0;
+        }
+    }
+}
+
 
 function onReady() {
     console.log("Ready to go!")
@@ -11,7 +55,6 @@ function onReady() {
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
     document.querySelector(".attacks").addEventListener("click", attackFungus);
-    console.log("hello")
     // 🧠 Remember
     // - Handle events that ->
     // - Updates state which is ->
@@ -21,29 +64,35 @@ onReady()
 
 function attackFungus(event) {
     let elementClicked = event.target;
-    let freakyFungusElement = document.querySelector(".freaky-fungus");
-    let apCost, HPdamage;
     if (elementClicked.classList.contains("arcane-scepter")) {
-        apCost = 12;
-        HPdamage = 14;
-    } else if (elementClickedclassList.contains("entangle")) {
-        apCost = 23;
-        HPdamage = 9;
+        cost = 12;
+        damage = 14;
+    } else if (elementClicked.classList.contains("entangle")) {
+        cost = 23;
+        damage = 9;
     } else if (elementClicked.classList.contains("dragon-blade")) {
-        apCost = 38;
-        HPdamage = 47;
+        cost = 38;
+        damage = 47;
     } else if (elementClicked.classlist.contains("star-fire")) {
-        apCost = 33;
-        HPdamage = 25;
+        cost = 33;
+        damage = 25;
     }
-    attackPoints -= apCost;
-    fungusHP -= HPdamage;
-    if (attackPoints <= 0) {
-        attackPoints = 0;
-        freakyFungusElement = replace("walk", "jump");
-        let attackButtonElements = document.getElementsByClassName("attack-btn");
-        for (i=0; i<attackButtonElements.length; i++) {
-            attackButtonElements[i].disabled = true;
-        }
+    fungusGameData.updateGameData(cost, damage);
+    console.log(fungusGameData);
+    renderGame();
+}
+
+
+
+// update DOM based on state values
+function renderGame() {
+    let freakyFungusElement = document.getElementById("")
+    // if bad fungus wins
+    if (fungusGameData.attackPoints === 0) {}
+
+    freakyFungusElement = replace("walk", "jump");
+    let attackButtonElements = document.getElementsByClassName("attack-btn");
+    for (i=0; i<attackButtonElements.length; i++) {
+        attackButtonElements[i].disabled = true;
     }
 }
